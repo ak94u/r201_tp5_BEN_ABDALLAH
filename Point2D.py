6ffcdb0921b52fd87840c9263dbbfa3ed25d797b
ak94u <akram.ben-abdallah@etu.u-pec.fr>
2026-03-30 14:40:20 +0200

    finalisation du développement de la class Point3D et petite modification et ajout de la méthode _eq__ dans la class Point2D

--- Point2D.py
+++ Point2D.py
@@ -23,15 +23,15 @@ class Point2D:
     def deplace(self, dx, dy):
         self.__x = self._get_x() + dx
         self.__y = self._get_y() + dy
-        
-        return(dx, dy)
     
     def distance(self, p):
+        dis = 0
         dx = self.__x - p._get_x()
         dy = self.__y - p._get_y()
         square_x = dx * dx
         square_y = dy * dy
-        return math.sqrt(square_x + square_y)
+        dis = square_x + square_y
+        return math.sqrt(dis)
     
     @property
     def rho(self):
@@ -42,20 +42,15 @@ class Point2D:
     
     @property
     def theta(self):
-        angle = 0
-        if self.__x == 0 :
-            return math.degrees(math.atan2(self.__y, self.__x))
-        else:
-            x = math.acos(self.__x/ self.rho)
-            y = math.asin(self.__y/ self.rho)
-            angle = y/x
-            return math.degrees(angle)
-        
+        return math.degrees(math.atan2(self.__y, self.__x))
 
     def affiche(self):
         x = self._get_x
         y = self._get_y
         print(f"x = {x} y = {y}")
+
+    def __eq__(self, p):
+        return p._get_x() == self.__x and p._get_y() == self.__y
         
     @classmethod
     def affiche_nb_point(cls):
@@ -67,17 +62,5 @@ class Point2D:
     def __str__(self):
         return "(" + str(self._get_x()) + ", " + str(self._get_y()) +")"
 
-p1 = Point2D(1, 2)
-print(p1)
 
-print(p1.__doc__())
-
-print(Point2D.__dict__)
-
-print(hasattr(p1, 'x'))
-print(hasattr(p1, 'z'))
-print(p1.rho)
-print(p1.theta)
-
-print(Point2D.affiche_nb_point())
 
--- Point3D.py
+++ Point3D.py
@@ -1,8 +1,66 @@
-import Point2D
+from Point2D import Point2D
+import math
+
 class Point3D(Point2D):
     nb_points = 0
 
-    def __init_(self, x, y, z):
-        super(x, y)
+    def __init__(self, x, y, z):
+        super().__init__(x, y)
         self.__z = z
         Point3D.nb_points += 1
+    
+    def _get_z(self):
+        return self.__z
+    
+    def _set_z(self, z):
+        self.__z = z
+
+    def deplace(self, dx, dy, dz):
+        super().deplace(dx, dy)
+        self.__z = self._get_z() + dz
+       
+    def distance(self, p):
+        d3D = 0
+        dz = self.__z - p._get_z()
+        square_z = dz ** 2
+        d2D = super().distance(p)
+        d3D = d2D**2 + square_z
+        return math.sqrt(d3D)
+    
+    @property
+    def rho(self):
+       """ Distance à l'origine (rayon sphérique) """
+       rho2D = super().rho()
+       square_z = self._get_z() ** 2
+       return math.sqrt(rho2D ** 2 + square_z)
+    
+    @property 
+    def r_xy(self): 
+        """ Projection dans le plan XY (rayon cylindrique) """
+        return math.sqrt(self.__x ** 2 + self.__y **2)
+    
+    def theta(self): 
+        """Angle azimutal dans le plan XY -- entre -180° et 180°"""
+        return math.degrees(math.atan2(self.__y, self.x))
+    
+    def phi(self):
+        """Angle polaire depuis l'axe Z (colatitude) - entre 0° et 180°"""
+        if self.rho == 0:
+            return 0.0
+        return math.degrees(math.acos(self.__z / self.rho))
+    
+    def affiche(self):
+        x = self.__x
+        y = self.__y
+        z = self.__z
+        print(f"x = {x}, y = {y}, z = {z}")
+
+    @classmethod
+    def affiche_nb_point(cls):
+        return cls.nb_points
+    
+    def __eq__(self, p):
+        return super().__eq__(p) and p._get_z() == self.__z
+    
+    def __str__(self):
+        return "(" + str(self._get_x()) + ", " + str(self._get_y()) + str(self._get_z()) + ")"
