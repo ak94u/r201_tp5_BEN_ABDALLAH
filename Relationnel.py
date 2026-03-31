cde7e51d5311333ff7c3f05a5fbcad5c2587272a
ak94u <akram.ben-abdallah@etu.u-pec.fr>
2026-03-31 22:08:42 +0200

    Ajout des getter et des setter et de la methode abstraite __str__

--- Relationnel.py
+++ Relationnel.py
@@ -6,4 +6,22 @@ class Relationnel:
         self.__q = q
         Relationnel.nb_Relationnel += 1
 
+    def _get_P(self):
+        return self.__p
+    
+    def _set_P(self, p):
+        self.__p = p
+    
+    def _get_Q(self):
+        return self.__q
+    
+    def _set_Q(self, q):
+        self.__q = q
+
+    def __str__(self):
+        if self._get_Q() == 0 :
+            return -1
+        
+        return str(self._get_P()) + "/" + str(self._get_Q())
+    
     
